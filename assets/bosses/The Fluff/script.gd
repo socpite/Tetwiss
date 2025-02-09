ecc7b2a5c9fbca66a5a24f0c9a051636fc928116
socpite <socpite@gmail.com>
2025-02-09 15:34:40 +0700

    added first boss

---
+++ assets/bosses/The Fluff/script.gd
@@ -0,0 +1,37 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func boss_start(game):
+	var tilemap: TileMap = game.get_node("TileMap")
+	var cover_height: int = tilemap.board_height/2
+	var cover_width: int = tilemap.board_length
+	
+	for i in cover_height:
+		for j in cover_width:
+			tilemap.set_cell(tilemap.layers.HUD, Vector2i(j, i), 0, Vector2i(0, 0), 8)
+	
+	tilemap.clear_layer(tilemap.layers.ghost_piece)
+	tilemap.ghost_piece_enabled = false
+
+func boss_process(game):
+	pass
+
+func boss_end(game):
+	var tilemap: TileMap = game.get_node("TileMap")
+	var cover_height: int = tilemap.board_height/2
+	var cover_width: int = tilemap.board_length
+	
+	for i in cover_height:
+		for j in cover_width:
+			tilemap.set_cell(tilemap.layers.HUD, Vector2i(j, i))
+	
+	tilemap.clear_layer(tilemap.layers.ghost_piece)
+	tilemap.ghost_piece_enabled = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
